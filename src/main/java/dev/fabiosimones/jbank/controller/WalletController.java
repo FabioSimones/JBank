4d1a40f36ba4c718a4ae1ca65c2a0825c4a289d1
Fábio simones <simones.fabio1994@gmail.com>
2025-06-24 19:31:51 -0300

    Tratando exceções no nível da @Controller.

--- src/main/java/dev/fabiosimones/jbank/controller/WalletController.java
+++ src/main/java/dev/fabiosimones/jbank/controller/WalletController.java
@@ -1,12 +1,11 @@
 package dev.fabiosimones.jbank.controller;
 
 import dev.fabiosimones.jbank.controller.dto.CreateWalletDTO;
+import dev.fabiosimones.jbank.exception.WalletDataAlreadyExistsException;
 import dev.fabiosimones.jbank.service.WalletService;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
@@ -25,5 +24,13 @@ public class WalletController {
         var wallet = walletService.createWaller(dto);
 
         return ResponseEntity.created(URI.create("/wallets/" + wallet.getWalletId().toString())).build();
+    }
+
+    @ExceptionHandler(WalletDataAlreadyExistsException.class)
+    public ProblemDetail handleWalletDataAlreadyExistsException(WalletDataAlreadyExistsException e){
+        var pd=  ProblemDetail.forStatus(422);
+        pd.setTitle("Wallet data already exists.");
+        pd.setDetail(e.getMessage());
+        return pd;
     }
 }
